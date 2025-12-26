f38e9f4a622a22b6e5318b9acbf4d4e2c23d4eab
Mahi <mushfikurahmaan@gmail.com>
2025-12-26 17:46:06 +0600

    Integrate Amazon S3 for media file storage - Add django-storages and boto3 to requirements - Configure S3 settings with environment variables - Add storages to INSTALLED_APPS

--- backend/settings.py
+++ backend/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'storages',  # For S3 storage
     'products',
     'orders',
 ]
@@ -140,3 +141,23 @@ STEADFAST_SECRET_KEY = os.environ.get('STEADFAST_SECRET_KEY')
 
 # Admin URL path (for security - use a custom path in production)
 ADMIN_URL_PATH = os.environ.get('ADMIN_URL_PATH', 'admin/')
+
+
+# AWS S3 Configuration
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERIFY = True
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+# Use S3 for media files (user uploads)
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Media files URL (S3)
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/'
