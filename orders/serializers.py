7e3c05f857f1d060906e60ed2b194dc82cf249a2
github-personal <mushfikurahmaan@gmail.com>
2026-01-24 16:52:20 +0600

    Update product color serializer and add db.sh to gitignore
    - Remove allow_null=True from product_color field in ProductOrderItemSerializer
    - Add db.sh to gitignore to protect sensitive credentials
    - Include migration for product_color field changes

--- orders/serializers.py
+++ orders/serializers.py
@@ -95,7 +95,7 @@ class ProductOrderItemSerializer(serializers.Serializer):
     product_id = serializers.IntegerField()
     product_name = serializers.CharField(required=False, allow_blank=True)
     product_size = serializers.CharField(max_length=50, required=False, allow_blank=True)
-    product_color = serializers.CharField(max_length=100, required=False, allow_blank=True, allow_null=True)
+    product_color = serializers.CharField(max_length=100, required=False, allow_blank=True)
     product_image = serializers.URLField(max_length=500, required=False, allow_blank=True, allow_null=True)
     quantity = serializers.IntegerField(min_value=1)
     unit_price = serializers.DecimalField(max_digits=10, decimal_places=2, required=False)
